6b1d091c8eb09bd5b83d16849897ccfbcfaac7c9
JayceFuller <jaycefuller05@protonmail.com>
2026-07-18 15:45:08 -0400

    Basics of page content outlined

---
+++ portfolio-ui/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./_Navbar.html")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("_Navbar file not found");
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById("navbar").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading navbar:", error));
+});
