6bbc7e664e1d0b52511c345a7dd76c13278b186a
JayceFuller <jaycefuller05@protonmail.com>
2026-07-19 17:50:17 -0400

    Reusable window-box created, links added to contacts page

--- portfolio-ui/js/main.js
+++ portfolio-ui/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("./_Navbar.html")
+  fetch("./shared/_Navbar.html")
     .then(response => {
       if (!response.ok) {
         throw new Error("_Navbar file not found");
@@ -8,6 +8,24 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(data => {
       document.getElementById("navbar").innerHTML = data;
+      initializeModeToggle();
     })
     .catch(error => console.error("Error loading navbar:", error));
 });
+
+
+function initializeModeToggle() {
+    const toggleButton = document.getElementById('mode-toggle');
+    const htmlElement = document.documentElement;
+    const savedMode = localStorage.getItem('mode');
+    const initialMode = savedMode || 'dark';
+    htmlElement.setAttribute('data-theme', initialMode);
+
+    toggleButton.addEventListener('click', () => {
+        const currentMode = htmlElement.getAttribute('data-theme');
+        const newMode = currentMode === 'dark' ? 'dark' : 'light';
+
+        htmlElement.setAttribute('data-theme', newMode);
+        localStorage.setItem('mode', newMode)
+    });
+}
---
+++ portfolio-ui/js/window-box.js
@@ -0,0 +1,57 @@
+class WindowBox extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        shadow.innerHTML = `
+            <style>
+                .window-box {
+                    background-color: lightgray;
+                    color: black;
+                    border: 2px solid;
+                    border-color: gray;
+                    box-shadow: inset;
+                }
+                .window-titlebar {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    padding: 10px;
+                    height: 20px;
+                    background-color: rgb(107, 25, 58);
+                    color: white;
+                }
+                .window-button {
+                    background-color: rgba(255, 255, 255, 0.5);
+                }
+                .window-content {
+                    padding-top: 10px;
+                    padding-bottom: 10px;
+                    padding-left: 25px;
+                    padding-right: 25px;
+                }
+                .digital-text {
+                    font-family: 'Courier New', Courier, monospace;
+                }
+            </style>
+
+            <div class="window-box">
+                <div class="window-titlebar">
+                    <p class="digital-text"><slot name="window-title"></slot></p>
+
+                    <div>
+                        <button class="window-button">–</button>
+                        <button class="window-button">&#9633;</button>
+                        <button class="window-button">x</button>
+                    </div>
+                </div>
+
+                <div class="window-content">
+                    <p class="digital-text"><slot name="window-content"></slot></p>
+                </div>
+            </div>
+        `
+    }
+}
+
+customElements.define('window-box', WindowBox)
