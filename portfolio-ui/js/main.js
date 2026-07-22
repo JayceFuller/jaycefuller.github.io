9b8ae5ed2b27b459b31db4c73f75685ae3c6a1ab
JayceFuller <jaycefuller05@protonmail.com>
2026-07-21 21:16:50 -0400

    Feature boxes created

--- portfolio-ui/js/main.js
+++ portfolio-ui/js/main.js
@@ -8,24 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(data => {
       document.getElementById("navbar").innerHTML = data;
-      initializeModeToggle();
     })
     .catch(error => console.error("Error loading navbar:", error));
 });
-
-
-function initializeModeToggle() {
-    const toggleButton = document.getElementById('mode-toggle');
-    const htmlElement = document.documentElement;
-    const savedMode = localStorage.getItem('mode');
-    const initialMode = savedMode || 'dark';
-    htmlElement.setAttribute('data-theme', initialMode);
-
-    toggleButton.addEventListener('click', () => {
-        const currentMode = htmlElement.getAttribute('data-theme');
-        const newMode = currentMode === 'dark' ? 'dark' : 'light';
-
-        htmlElement.setAttribute('data-theme', newMode);
-        localStorage.setItem('mode', newMode)
-    });
-}
---
+++ portfolio-ui/shared/html-elements/feature-element.js
@@ -0,0 +1,58 @@
+class FeatureElement extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        shadow.innerHTML = `
+            <style>
+                .feature-box {
+                    animation: floatUp 0.8s ease-out forwards; 
+                    background-color: rgb(225, 225, 225);;
+                    color: black;
+                    border: 2px solid;
+                    border-color: rgb(107, 25, 58);;
+                    border-radius: 4px;
+                    box-shadow: inset;
+                    width: 350px;
+                    height: 400px;
+                }
+                    .feature-box:hover {
+                        transform: translateY(-4px);
+                        box-shadow: 0 4px 12px rgba(0,0,0,0.25);
+                        transition: all 0.2s ease-in-out;
+                    }
+                .feature-titlebar {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    justify-content: center;
+                    padding: 10px;
+                    height: 20px;
+                    background-color: rgb(107, 25, 58);
+                    color: white;
+                }
+                .feature-content {
+                    padding-top: 10px;
+                    padding-bottom: 10px;
+                    padding-left: 25px;
+                    padding-right: 25px;
+                }
+                .digital-text {
+                    font-family: 'Courier New', Courier, monospace;
+                }
+            </style>
+
+            <div class="feature-box">
+                <div class="feature-titlebar">
+                    <p class="digital-text"><slot name="feature-title"></slot></p>
+                </div>
+
+                <div class="feature-content">
+                    <slot name="preview-img"></slot>
+                    <p class="digital-text"><slot name="feature-content"></slot></p>
+                </div>
+            </div>
+        `;
+    }
+}
+customElements.define('feature-box', FeatureElement)
